058084652c7198d9a8f1ce6f6b45279dc612b7bf
ebbbe <e.etwobe@gmail.com>
2022-05-24 17:43:20 +0200

    Add vuetify

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
+    "eslint-plugin-vue",
   ],
   env: {
     "vue/setup-compiler-macros": true,
