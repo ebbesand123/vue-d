57baa105b4fb9e7af60e73a9254d440105b80d90
ebbbe <e.etwobe@gmail.com>
2022-05-27 20:10:08 +0200

    Remove vuetify

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,6 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-    }),
   ],
   define: { 'process.env': {} },
   resolve: {
