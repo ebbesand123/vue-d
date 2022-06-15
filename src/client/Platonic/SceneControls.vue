f6c937ae460b655150d93d121e44398ad6515bd3
ebbbe <e.etwobe@gmail.com>
2022-06-15 18:37:10 +0200

    Add scene control

--- src/client/Platonic/SceneControls.vue
+++ src/client/Platonic/SceneControls.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="scene-controls">
     <h2>Scene Controls</h2>
-    <div v-for="(scn, i) in refScene.scene" :key="scn.uuid">
+    <div v-for="scn in refScene.scene" :key="scn.uuid">
       <n-space vertical>
-        <span> Index: {{ i }}</span>
-        <span> Background: {{ scn.background }}</span>
+        <h3>Background color</h3>
+        <n-color-picker
+          :value="getColor(scn)"
+          :default-value="getColor(scn)"
+          :modes="['hex']"
+          :show-alpha="false"
+          @update:value="
+            (value) => {
+              updateColor(value, scn);
+            }
+          "
+        ></n-color-picker>
       </n-space>
     </div>
   </div>
@@ -12,11 +22,20 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { NColorPicker, NSpace } from "naive-ui";
-import { Scene } from "three";
+import { Color, Scene } from "three";
 const props = defineProps<{
   scene: Scene[];
 }>();
 const refScene = ref(props);
+function updateColor(hex: string, scene: Scene) {
+  const color = new Color(hex.toUpperCase());
+  scene.background = color;
+}
+function getColor(scene: Scene) {
+  if (scene.background instanceof Color) {
+    return "#" + scene.background.clone().getHexString().toUpperCase();
+  }
+}
 </script>
 <style scoped>
 .scene-controls {
